2fe7f7d1540e86b1620adc17b45a6c58f0479f44
ALEXANDRA ENCARNACION <alex1102e@gmail.com>
2025-01-10 17:12:43 +0000

    Create DigitButton

--- src/App.js
+++ src/App.js
@@ -1,20 +1,45 @@
 import './styles.css';
 import './App.css';
+import DigitButton from './DigitButton'
+import { useReducer } from 'react';
+
+export const ACTIONS = {
+  ADD_DIGIT: 'add-digit',
+  CHOOSE_OPERATION: 'choose-operation',
+  CLEAR: 'clear',
+  DELETE_DIGIT: 'delete-digit',
+  EVALUATE: 'evaluate'
+}
+
+function reducer(state, {type, payload}){
+  switch(type){
+    case ACTIONS.ADD_DIGIT:
+      return {
+        ...state,
+        currentOperand: `${state.currentOperand || ""}${payload.digit}`
+      }
+  }
+}
 
 function App() {
+  const [{currentOperand, previousOperand, operation}, dispatch] = useReducer(
+    reducer, 
+    {}
+  )
+  
   return(
     <div className="calculator-grid">
       <div className="output">
         <div className="previous-operand">
-
+          {previousOperand} {operation}
         </div>
         <div className="current-operand">
-          
+          {currentOperand}
         </div>
       </div>
       <button className="span-two">AC</button>
       <button>DEL</button>
-      <button>÷</button>
+      <DigitButton digit="÷" dispatch={dispatch} />
       <button>1</button>
       <button>2</button>
       <button>3</button>
