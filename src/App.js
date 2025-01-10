1fda44b5a99456502cd0cd5ba2166c8699773dbd
ALEXANDRA ENCARNACION <alex1102e@gmail.com>
2025-01-10 17:42:24 +0000

    Add all clear functionality

--- src/App.js
+++ src/App.js
@@ -25,6 +25,8 @@ function reducer(state, {type, payload}){
         ...state,
         currentOperand: `${state.currentOperand || ""}${payload.digit}`
       }
+    case ACTIONS.CLEAR:
+      return {}
   }
 }
 
@@ -44,7 +46,7 @@ function App() {
           {currentOperand}
         </div>
       </div>
-      <button className="span-two">AC</button>
+      <button className="span-two" onClick={() => dispatch({ type: ACTIONS.CLEAR })}>AC</button>
       <button>DEL</button>
       <OperationButton operation="÷" dispatch={dispatch} />
       <DigitButton digit="1" dispatch={dispatch} />
